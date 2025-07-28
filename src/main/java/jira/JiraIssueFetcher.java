6a4cf07665f2c26dbc6bbe262a940e4bbcd35798
BaurelTanekam <baurel.tanekam@medien-systempartner.de>
2025-07-28 13:28:34 +0200

    [MSPINTERN-2729] Maven dependecy Json updated

--- src/main/java/jira/JiraIssueFetcher.java
+++ src/main/java/jira/JiraIssueFetcher.java
@@ -1,5 +1,10 @@
 package jira;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JiraIssueFetcher {
-    
+    private CloseableHttpClient httpClient;
+    private ObjectMapper objectMapper;
 }
